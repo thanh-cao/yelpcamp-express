1381fa747a457d7f6c0dae5da61d916ec52b969a
Thanh <55603278+thanh-cao@users.noreply.github.com>
2021-12-31 16:27:01 +0100

    add user model and configure passport

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+
 const ExpressError = require('./utils/ExpressError')
 
 require('dotenv').config();
@@ -37,6 +41,12 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
