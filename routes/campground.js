e8fdb431258b8c82cca8cca75547929d78a35bad
Thanh <55603278+thanh-cao@users.noreply.github.com>
2022-01-16 17:17:20 +0100

    refactor: chaining routes

--- routes/campground.js
+++ routes/campground.js
@@ -5,19 +5,26 @@ const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
+// refactored with chaining
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
-router.get('/', catchAsync(campgrounds.index));
-
+// /new route needs to be before /:id route otherwise it will be caught by /:id route which will thinks new is an id    
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
-router.get('/:id', catchAsync(campgrounds.showCampground));
+router.route('/:id')
+.get(catchAsync(campgrounds.showCampground))
+.put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+.delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+// old routes
+// router.get('/', catchAsync(campgrounds.index));
+// router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+// router.get('/:id', catchAsync(campgrounds.showCampground));
+// router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
+// router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,20 @@ const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
 const users = require('../controllers/users');
 
+// refactored with chaining
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.get('/register', users.renderRegister);
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLogin)
-
-router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login);
+// old routes
+// router.get('/register', users.renderRegister);
+// router.post('/register', catchAsync(users.register));
+// router.get('/login', users.renderLogin)
+// router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
 router.get('/logout', users.logout)
 
