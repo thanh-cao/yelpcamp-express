afeddbec8562e7bb7d4f5c2dcade4ce40d0f473f
Thanh <55603278+thanh-cao@users.noreply.github.com>
2022-01-16 15:44:50 +0100

    add isAuthor middleware to give edit/delete campground permission to its respective author

--- middleware.js
+++ middleware.js
@@ -1,9 +1,22 @@
+const Campground = require('./models/campground');
+
 // isLoggedIn middleware to check if user is logged in before allowing them to do certain actions like create new/edit/delete campgrounds etc.
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl
         req.flash('error', 'You must be signed in first!');
         return res.redirect('/login');
+    }
+    next();
+}
+
+// isAuthor middleware to check if user is the author of the campground to give them permission to edit/delete it.
+module.exports.isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
     }
     next();
 }
