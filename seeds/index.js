fc154460128f5513f12134ca51c408068c8be7c9
Thanh <55603278+thanh-cao@users.noreply.github.com>
2022-01-16 19:35:41 +0100

    reseeding data with img links from cloudinary

--- seeds/index.js
+++ seeds/index.js
@@ -21,7 +21,16 @@ const seedDB = async () => {
             author: '61e2d7bb5191c5f55e4bdeda',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dukzszdua/image/upload/v1642357633/YelpCamp/photo-1455763916899-e8b50eca9967_vuswqe.jpg',
+                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dukzszdua/image/upload/v1642357633/YelpCamp/photo-1573145881456-0708c72340ca_m5gjhp.jpg',
+                    filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
+                }
+            ],
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price
         })
