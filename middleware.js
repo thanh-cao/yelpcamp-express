d219c4ca0b7d4b2e8439872c4f6204bd4a9df394
Thanh <55603278+thanh-cao@users.noreply.github.com>
2022-01-16 14:21:12 +0100

    add isLoggedIn middleware to limit access create/edit/delete to non-loggedin users

---
+++ middleware.js
@@ -0,0 +1,9 @@
+// isLoggedIn middleware to check if user is logged in before allowing them to do certain actions like create new/edit/delete campgrounds etc.
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl
+        req.flash('error', 'You must be signed in first!');
+        return res.redirect('/login');
+    }
+    next();
+}
