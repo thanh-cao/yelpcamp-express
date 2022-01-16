e234e113aa8fbf5dbc2c518bba1afc307596d0d3
Thanh <55603278+thanh-cao@users.noreply.github.com>
2022-01-16 20:01:32 +0100

    create image virtual for thumbnail

--- models/campground.js
+++ models/campground.js
@@ -2,12 +2,19 @@ const mongoose = require('mongoose');
 const Review = require('./review')
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+// create a virutal property called thumbnail for image to be displayed in the edit page instead of storing another data in the database
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [{
-        url: String,
-        filename: String
-    }],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
@@ -16,9 +23,9 @@ const CampgroundSchema = new Schema({
         ref: 'User'
     },
     reviews: [{
-        type: Schema.Types.ObjectId,
-        ref: "Review"
-    }]
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }]
 });
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
