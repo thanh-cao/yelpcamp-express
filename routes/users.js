a132c3ac3f59d11c827a1a4cb25d0aa90272f9d2
Thanh <55603278+thanh-cao@users.noreply.github.com>
2022-01-16 14:07:12 +0100

    add login and logout routes

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
-
+const passport = require('passport');
 
 router.get('/register', (req, res) => {
     res.render('auth/register');
@@ -16,12 +16,29 @@ router.post('/register', catchAsync(async (req, res, next) => {
         req.login(registeredUser, err => {
             if (err) return next(err);
             req.flash('success', 'Welcome to Yelp Camp!');
-            req.redirect('/campgrounds');
+            res.redirect('/campgrounds');
         })
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('register');
     }
 }));
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+})
+
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
+    req.flash('success', 'Welcome back!');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+});
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success', 'Goodbye!');
+    res.redirect('/campgrounds');
+})
 
 module.exports = router;
